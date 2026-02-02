38f65f4134e0160c0b0082aac53598a5c894c21a
shuoisme <80932224+shuoisme@users.noreply.github.com>
2026-02-02 14:19:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def check_stock():
         elif is_tail_time: title = "☕ [尾盤]"
         elif is_close_time: title = "🌅 [收盤]"
         
-        full_msg = f"{title} 行情 (金融精算版)\n" + "-"*18 + "\n" + "\n\n".join(report_msgs)
+        full_msg = f"{title} 行情 (精準)\n" + "-"*18 + "\n" + "\n\n".join(report_msgs)
         send_line_push(full_msg)
         print(f"✅ 已發送: {title}")
 
